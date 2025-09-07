22ad9b3695b1e809832ddf0c5655ee2e39ff1ec8
Grant <grant@graphenedata.com>
2025-09-07 09:18:02 -0500

    Add other chart types

---
+++ ui/components/areaChart.svelte
@@ -0,0 +1,24 @@
+<svelte:options customElement="graphene-areachart" />
+<script>
+  import {AreaChart} from '@evidence-dev/core-components'
+
+  let callback
+  let data = {
+    isQuery: true,
+    fetch: async () => {
+      await Promise.resolve()
+      let dt = await window.$GRAPHENE.query($$props.data)
+      dt.dataLoaded = true
+      callback(dt)
+    },
+    subscribe: (cb) => callback = cb
+  }
+
+  $: spreadProps = {
+		...Object.fromEntries(Object.entries($$props).filter(([, v]) => v !== undefined))
+	};
+</script>
+
+<style></style>
+
+<AreaChart {...spreadProps} data={data} />
