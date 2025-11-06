d4296a27d9083b2b5179a6e6878dad291ca27de9
Grant <grant@graphenedata.com>
2025-11-06 12:56:39 -0600

    set up turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,18 @@
 import {defineConfig} from 'drizzle-kit'
+import 'drizzle-orm'
+
+let dbCredentials: any = {}
+
+if (process.env.TURSO_DATABASE_URL) {
+  dbCredentials.url = process.env.TURSO_DATABASE_URL
+  dbCredentials.authToken = process.env.TURSO_AUTH_TOKEN
+} else {
+  dbCredentials.url = 'file:cloud.db'
+}
 
 export default defineConfig({
   schema: './schema.ts',
   out: './migrations',
-  dialect: 'sqlite',
-  dbCredentials: {
-    url: process.env.CLOUD_DB_PATH ?? './cloud.db',
-  },
+  dialect: 'turso',
+  dbCredentials,
 })
--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,16 @@
-import SqliteDatabase from 'better-sqlite3'
-import type {Database as SqliteInstance} from 'better-sqlite3'
-import {drizzle, type BetterSQLite3Database} from 'drizzle-orm/better-sqlite3'
+import {type Client, createClient} from '@libsql/client'
+import {drizzle, type LibSQLDatabase} from 'drizzle-orm/libsql'
 import * as schema from '../schema.ts'
-import {fileURLToPath} from 'url'
-import path from 'path'
 
-export type CloudDatabase = BetterSQLite3Database<typeof schema>
+export type CloudDatabase = LibSQLDatabase<typeof schema>
 
 let dbInstance: CloudDatabase | undefined
-let sqliteInstance: SqliteInstance | undefined
+let sqliteInstance: Client | undefined
 
 export function getDb (): CloudDatabase {
   if (dbInstance) return dbInstance
-
-  let dbPath = getDbPath()
-  sqliteInstance = new SqliteDatabase(dbPath)
+  let url = process.env.NODE_ENV == 'test' ? ':memory' : process.env.TURSO_DATABASE_URL!
+  sqliteInstance = createClient({url, authToken: process.env.TURSO_AUTH_TOKEN})
   dbInstance = drizzle(sqliteInstance, {schema})
   return dbInstance
 }
@@ -23,10 +19,4 @@ export function resetDb (): void {
   dbInstance = undefined
   sqliteInstance?.close()
   sqliteInstance = undefined
-}
-
-function getDbPath (): string {
-  if (process.env.NODE_ENV === 'test') return ':memory:'
-  if (process.env.CLOUD_DB_PATH) return process.env.CLOUD_DB_PATH
-  return path.resolve(fileURLToPath(import.meta.url), '../../cloud.db')
 }
