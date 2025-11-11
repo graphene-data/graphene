00da03d7935bb82faf245cc46d9093438c224f69
Grant <grant@graphenedata.com>
2025-11-11 11:13:39 -0600

    Also accept connected app jwts
    In addition to stytch session jwts

--- server/auth.ts
+++ server/auth.ts
@@ -14,22 +14,27 @@ export function setAuthOverride (auth: AuthContext | null) {
 
 export async function checkAuth (req: FastifyRequest) {
   req.auth = null
+
   if (process.env.NODE_ENV === 'test' && authOverride) {
     req.auth = authOverride
     return
   }
 
-  let jwt = req.cookies['stytch_session_jwt']
-  if (!jwt) return
+  let bearer = req.headers['authorization']
+  if (bearer) {
+    let claims = await getStytch().idp.introspectTokenLocal(bearer.replace(/^bearer /i, ''))
+    req.auth = {userId: claims.subject, orgId: claims.organization.organization_id}
+  }
 
-  let auth = await getStytch().sessions.authenticateJwt({session_jwt: jwt})
-  let session = auth.member_session
+  let session_jwt = req.cookies['stytch_session_jwt']
+  if (session_jwt) {
+    let auth = await getStytch().sessions.authenticateJwt({session_jwt: jwt})
+    let session = auth.member_session
+    if (!session) return
+    req.auth = {userId: session.member_id, orgId: session.organization_id}
+  }
 
   // TODO check org matches subdomain
-  // TODO check user belongs to org
-
-  if (!session) req.auth = null
-  else req.auth = {userId: session.member_id, orgId: session.organization_id}
 }
 
 export function ensureUser (req: FastifyRequest, reply: FastifyReply) {
@@ -46,6 +51,7 @@ function getStytch (): B2BClient {
   stytchClient ||= new B2BClient({
     project_id: process.env.STYTCH_PROJECT_ID ?? '',
     secret: process.env.STYTCH_SECRET ?? '',
+    custom_base_url: process.env.STYTCH_DOMAIN ?? '',
   })
   return stytchClient
 }
