56e7b455d0c2f3020ba0e1b509bc1326c0821bf7
Grant <grant@graphenedata.com>
2025-11-21 20:57:24 -0600

    fix check exit status

--- cli/check.ts
+++ cli/check.ts
@@ -112,7 +112,7 @@ export async function check (options: CheckOptions): Promise<boolean> {
     log('Screenshot saved to', screenshotPath)
   }
 
-  return !!errors.length
+  return errors.length == 0
 }
 
 async function sendCheckRequest ({host, pageUrl, chart}) {
