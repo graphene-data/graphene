443e3d94c3bae9f87fcf06c5a3a4128aab7ab174
Grant <grant@graphenedata.com>
2025-12-01 11:17:02 -0600

    Read bigquery projectId out of credential files, if present.

--- cli/connections/bigQuery.ts
+++ cli/connections/bigQuery.ts
@@ -1,6 +1,7 @@
 import {BigQuery, BigQueryDate, BigQueryTimestamp, type BigQueryOptions} from '@google-cloud/bigquery'
 import {type QueryConnection} from './types.ts'
 import {config} from '../../lang/config.ts'
+import {readFileSync} from 'fs'
 
 // You can also set GOOGLE_APPLICATION_CREDENTIALS to point at a service account key file
 
@@ -15,11 +16,18 @@ export class BigQueryConnection implements QueryConnection {
       options.credentials = parsed
     }
 
+    if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+      let tmp = JSON.parse(readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, {encoding: 'utf-8'}))
+      options.projectId = tmp.project_id
+    }
+
     if (!options.projectId) throw new Error('projectId must be set in config or provided in service account credentials')
+    this.projectId = options.projectId
     this.client = new BigQuery({...options, userAgent: 'Graphene'})
+    this.defaultNamespace = config.namespace
   }
 
-  async runQuery (sql: string) {
+  async runQuery (sql: string): Promise<QueryResult> {
     let [job] = await this.client.createQueryJob({query: sql, useLegacySql: false})
     let [rows] = await job.getQueryResults({maxResults: 10000})
     let metadata = job.metadata || (await job.getMetadata())[0]
