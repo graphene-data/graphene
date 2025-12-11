769d0d93dd4e7d94bea877c5e730dd7f2e8b22c4
Grant <grant@graphenedata.com>
2025-12-11 17:30:23 -0600

    Use threads when debugging tests
    Otherwise you get zombies

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     testTimeout: process.env.GRAPHENE_DEBUG ? 0 : 10_000,
+    pool: process.env.GRAPHENE_DEBUG ? 'threads' : 'forks', // otherwise, cmd+c when debugging will zombie the worker
     environment: 'node',
     include: ['tests/*.test.ts'],
     setupFiles: ['tests/setup.ts'],
