2da5e80cb609e82db3db8e6c1d60f85bc6164d79
Grant <grant@graphenedata.com>
2025-12-11 22:08:30 -0600

    fix verify step of publish script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -16,8 +16,8 @@ if (!['major', 'minor', 'patch'].includes(bumpLevel)) {
 if (!process.env.VSCE_PAT) throw new Error('VSCE_PAT required')
 if (!process.env.OVSX_PAT) throw new Error('OVSX_PAT required')
 
-await $`npx vsce verify-pat`
-await $`npx ovsx verify-pat`
+await $`(cd vscode && npx vsce verify-pat)`
+await $`(cd vscode && npx ovsx verify-pat)`
 
 let branch = (await $`git rev-parse --abbrev-ref HEAD`).stdout.trim()
 if (branch !== 'main') throw new Error('Publishing must run on main')
