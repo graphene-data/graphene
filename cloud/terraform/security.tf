4a858d08c7c1e300029de3013f77aae024d2453f
Grant <grant@graphenedata.com>
2026-01-21 16:53:44 -0600

    remove redundant standards

--- cloud/terraform/security.tf
+++ cloud/terraform/security.tf
@@ -294,12 +294,6 @@ resource "aws_securityhub_finding_aggregator" "main" {
   depends_on   = [aws_securityhub_account.main]
 }
 
-# Enable AWS Foundational Security Best Practices standard
-resource "aws_securityhub_standards_subscription" "aws_foundational" {
-  standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/aws-foundational-security-best-practices/v/1.0.0"
-  depends_on    = [aws_securityhub_account.main]
-}
-
 # Enable CIS AWS Foundations Benchmark
 resource "aws_securityhub_standards_subscription" "cis" {
   standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
