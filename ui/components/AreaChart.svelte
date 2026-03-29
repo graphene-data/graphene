a8cd937b14d259aaaa5a50b1a3ca248a5a0b710b
Grant <me@grantmarvin.com>
2026-03-29 00:12:40 -0500

    fix imports

--- ui/components/AreaChart.svelte
+++ ui/components/AreaChart.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import ECharts from './ECharts.svelte'
-  import type {EChartsConfig2} from '../component-utilities/types.ts'}},{
+  import type {EChartsConfig2} from '../component-utilities/types.ts'
 
   interface Props {
     data: string | {rows?: any[]; fields?: any[]}
