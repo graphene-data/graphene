48eeae8a6c094e3840083e4083b45d21e035099f
Grant <me@grantmarvin.com>
2026-03-28 21:07:21 -0500

    Remove old viz code

--- ui/component-utilities/getSortedData.js
+++
@@ -1,9 +0,0 @@
-export default function getSortedData(data, col, isAsc) {
-  let res = [...data].sort((a, b) => {
-    return (a[col] < b[col] ? -1 : 1) * (isAsc ? 1 : -1)
-  })
-  if (Array.isArray(data?._evidenceColumnTypes)) {
-    res._evidenceColumnTypes = data._evidenceColumnTypes
-  }
-  return res
-}
